58c04e2ac8025fc4b7c8fd396939c0cba940f7d1
sk <sukh2006on@gmail.com>
2023-03-11 21:20:38 +0800

    wizard

--- addons/mylib/models/lib_book.py
+++ addons/mylib/models/lib_book.py
@@ -1,6 +1,12 @@
 from odoo import models, fields, api
 from datetime import timedelta
 
+from odoo.exceptions import UserError
+from odoo.tools.translate import _
+
+import logging
+logger = logging.getLogger(__name__)
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
     published_book_ids = fields.One2many('library.book', 'publisher_id', string='Published Books')
@@ -75,7 +81,6 @@ class LibraryBook(models.Model):
                 book.age_days = delta.days
             else:
                 book.age_days = 0
-
     def _inverse_age(self):
         today = fields.Date.today()
         for book in self.filtered('date_release'):
@@ -105,35 +110,127 @@ class LibraryBook(models.Model):
         models = self.env['ir.model'].search([('field_id.name', '=', 'message_ids')])
         return [(x.model, x.name) for x in models]
 
+    # state = fields.Selection(
+    #        [('draft', 'Not Available'),
+    #         ('available', 'Available'),
+    #         ('lost', 'Lost')],
+    #         'State', default="draft")
     state = fields.Selection(
-           [('draft', 'Not Available'),
-            ('available', 'Available'),
-            ('lost', 'Lost')],
-            'State', default="draft")
+        [('available', 'Available'),
+         ('borrowed', 'Borrowed'),
+         ('lost', 'Lost')],
+        'State', default="available")
 
-    @api.model
-    def is_allowed_transition(self, old_state, new_state):
-        allowed = [('draft', 'available'),
-                   ('available', 'borrowed'),
-                   ('borrowed', 'available'),
-                   ('available', 'lost'),
-                   ('borrowed', 'lost'),
-                   ('lost', 'available')]
-        return (old_state, new_state) in allowed
+    # @api.model
+    # def is_allowed_transition(self, old_state, new_state):
+    #     allowed = [('draft', 'available'),
+    #                ('available', 'borrowed'),
+    #                ('borrowed', 'available'),
+    #                ('borrowed', 'ongoing'),
+    #                ('available', 'lost'),
+    #                ('borrowed', 'lost'),
+    #                ('lost', 'available')]
+    #     return (old_state, new_state) in allowed
 
-    def change_state(self, new_state):
-        for book in self:
-            if book.is_allowed_transition(book.state,new_state):
-                book.state = new_state
-            else:
-                continue
+    # def change_state(self, new_state):
+    #     for book in self:
+    #         if book.is_allowed_transition(book.state,new_state):
+    #             book.state = new_state
+    #         else:
+    #             message = _('Moving from %s to %s is not allowd') % (book.state, new_state)
+    #             raise UserError(message)
+
+    # def make_available(self):
+    #     self.change_state('available')
+    # def make_borrowed(self):
+    #     self.change_state('borrowed')
+    # def make_lost(self):
+    #     self.change_state('lost')
 
     def make_available(self):
-        self.change_state('available')
+        self.ensure_one()
+        self.state = 'available'
+
     def make_borrowed(self):
-        self.change_state('borrowed')
+        self.ensure_one()
+        self.state = 'borrowed'
+
     def make_lost(self):
-        self.change_state('lost')
+        self.ensure_one()
+        self.state = 'lost'
+
+    def make_lost(self):
+        self.ensure_one()
+        self.state = 'lost'
+        if not self.env.context.get('avoid_deactivate'):    #end rent-s duudsan func avoid_deactivate = true damjuulsan bga.
+            self.active = False
+    def log_all_library_members(self):
+        library_member_model = self.env['library.member']
+        all_members = library_member_model.search([])
+        print("ALL MEMBERS:", all_members)
+        return True
+
+    def change_release_date(self):
+        self.ensure_one()
+        self.date_release = fields.Date.today()
+
+    def find_book(self):
+        domain = [
+            '|',
+            # '&',
+            ('name', 'ilike', 'bat'),
+            # ('category_id.name', 'ilike', ''),
+            #  '&',
+            ('name', 'ilike', 'dorj'),
+            # ('category_id.name', 'ilike', '')
+             ]
+        books = self.search(domain)
+        return book
+
+    def filter_books(self):
+        all_books = self.search([])
+        filtered_books = self.books_with_multiple_authors(all_books)
+        logger.info('Filtered Books: %s', filtered_books)
+        return true
+
+    @api.model
+    def books_with_multiple_authors(self, all_books):
+        def predicate(book):
+            if len(book.author_ids) > 1:
+                return True
+        return all_books.filtered(predicate)
+
+    def book_rent(self):
+        self.ensure_one()
+        if self.state != 'available':
+            raise UserError(_('Book is not available for renting'))
+        rent_as_superuser = self.env['library.book.rent'].sudo()        #yamr neg hailt hiigdeq uchir empty recordset irne gejishu.
+        rent_as_superuser.create({
+            'book_id': self.id,
+            'borrower_id': self.env.user.partner_id.id,
+        })
+        # public_user = self.env.ref('base.public_user')                #sudogiin orond ingej oor usereer uildel hiij bolhin shig bn.
+        # public_book = self.env['library.book'].with_user(public_user)
+        # public_book.search([('name', 'ilike', 'cookbook')])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class LibraryMember(models.Model):
     _name = 'library.member'
     _inherits = {'res.partner': 'partner_id'}
@@ -143,3 +240,57 @@ class LibraryMember(models.Model):
     date_end = fields.Date('Termination Date')
     member_number = fields.Char()
     date_of_birth = fields.Date('Date of birth')
+class LibraryBookRent(models.Model):
+    _name = 'library.book.rent'
+
+    book_id = fields.Many2one('library.book', 'Book', required=True)
+    borrower_id = fields.Many2one('res.partner', 'Borrower', required=True)
+    state = fields.Selection([('ongoing', 'Ongoing'), ('returned', 'Returned'), ('lost', 'Lost')], 'State', default='ongoing', required=True)
+    rent_date = fields.Date(default=fields.Date.today)
+    return_date = fields.Date()
+
+    @api.model
+    def create(self, vals):
+        book_rec = self.env['library.book'].browse(vals['book_id'])  # returns record set from for given id
+        book_rec.make_borrowed()
+        return super(LibraryBookRent, self).create(vals)
+
+    def book_return(self):
+        self.ensure_one()
+        self.book_id.make_available()
+        self.write({
+            'state': 'returned',
+            'return_date': fields.Date.today()
+        })
+
+    def book_lost(self):
+        self.ensure_one()
+        self.sudo().state = 'lost'
+        book_with_different_context = self.book_id.with_context(avoid_deactivate=False)  #ene bookiin make lost ruu avoid_deactivate=True utga damjuulj bn.
+        book_with_different_context.sudo().make_lost()      #tegeed bok ni lost tolovteigoos gadna avtive ni false bolhin bn.
+        # new_context = self.env.context.copy()
+        # new_context.update({'avoid_deactivate': True})
+
+class LibraryRentWizard(models.TransientModel):
+    _name = 'library.rent.wizard'
+    borrower_id = fields.Many2one('res.partner',    string='Borrower')
+    book_ids = fields.Many2many('library.book',    string='Books')
+
+    def add_book_rents(self):
+        rentModel = self.env['library.book.rent']
+        for wiz in self:
+            for book in wiz.book_ids:
+                rentModel.create({                      #ene shuud rent deer bichleg nemj bn.
+                    'borrower_id': wiz.borrower_id.id,
+                    'book_id': book.id
+                })
+
+
+
+
+
+
+
+
+
+
--- addons/mylib/models/library_book_categ.py
+++ addons/mylib/models/library_book_categ.py
@@ -6,6 +6,7 @@ class BookCategory(models.Model):
     _parent_name = "parent_id"
 
     name = fields.Char('Category')
+    description = fields.Text('Description')
     parent_id = fields.Many2one('library.book.category', string='Parent Category', ondelete='restrict', index=True)
     child_ids = fields.One2many('library.book.category', 'parent_id', string='Child Categories')
 
@@ -13,6 +14,27 @@ class BookCategory(models.Model):
     def _check_hierarchy(self):
         if not self._check_recursion():
             raise models.ValidationError('Error! You cannot create recursive categories.')
+
+    def create_categories(self):
+        categ1 = {
+            'name': 'Child category 3',
+            'description': 'Description for child 3'
+        }
+        categ2 = {
+            'name': 'Child category 4',
+            'description': 'Description for child 4'
+        }
+        parent_category_val = {
+            'name': 'Parent category 3',
+            'description': 'Description for parent category',
+            'child_ids': [
+                (6, 0, categ1),
+                (6, 0, categ2),
+            ]
+        }
+        # Total 3 records (1 parent and 2 child) will be craeted in library.book.category model
+        record = self.env['library.book.category'].create(parent_category_val)
+        return True
 
 
 
